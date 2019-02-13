d37668cc723fd5914892037ed60cc3e8af4441dc
Naga Chaitanya Konada <knc@outlook.in>
2019-02-13 23:58:15 +0530

    added content to home pages.

--- src/demo/About.js
+++ src/demo/About.js
@@ -3,6 +3,7 @@ import React from 'react';
 const About = () => (
     <div>
         <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, fugiat id? Dolor vero earum itaque repudiandae, aspernatur perspiciatis eum qui?</p>
     </div>
 );
 
--- src/demo/Home.js
+++ src/demo/Home.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Home = () => (
     <div>
         <h2>Home</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt qui quam facilis ratione saepe rem necessitatibus nobis, laborum similique? Consectetur.</p>
     </div>
 );
 
