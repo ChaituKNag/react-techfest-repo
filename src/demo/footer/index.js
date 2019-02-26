203b43166c82e2feef91695057af87e576f488f2
Shaik Gulshan <sgulshan@deloitte.com>
2019-02-26 12:44:27 +0530

    Footer component changes

--- src/demo/footer/index.js
+++ src/demo/footer/index.js
@@ -1,11 +1,26 @@
-import React, {Component} from 'react';
+import React from "react";
 
-import './index.scss';
+import "./index.scss";
 
- const FooterComponent=()=>(
+const FooterComponent = () => (
     <div>
-            FooterComponent
-        </div>
+        <footer className="footer-component">
+            <div className="footer-title">
+                E-Commerce
+            </div>
+            <div className="footer-sub-title">
+                Platform
+            </div>
+            <nav className="footer-nav">
+                <ul>
+                    <li>Home</li>
+                    <li>Product Information</li>
+                    <li>Privacy Policy</li>
+                    <li>About us</li>
+                </ul>
+            </nav>
+        </footer>
+  </div>
 );
 
 export default FooterComponent;
