987049f991e2ade0046601bda9d66e8872e20a12
Chetan Lakhanpal <clakhanpal@deloitte.com>
2019-02-27 01:34:41 +0530

    updated landing page

--- src/app/pages/Landing.js
+++ src/app/pages/Landing.js
@@ -1,16 +1,8 @@
 import React from 'react';
-import styled from 'styled-components';
 import Container from '../components/base/Container';
 import Banner from '../components/Banner';
 import Categories from "../components/Category/Categories";
 import { Products } from "../components/Products/Products";
-import Button from '../components/base/Button';
-import ProductCard from '../components/Products/ProductCard';
-import { PRIMARY, SECONDARY } from '../../constants/properties';
-
-const ProductsList = styled.div`
-  display: flex;
-`
 
 const Landing = () => (
   <div>
@@ -19,15 +11,10 @@ const Landing = () => (
       <h2>Landing Page listing comes here</h2>
       <div>
         <aside><Categories /></aside>
-        <section><Products /></section><br />
-
-        <Button type={PRIMARY}>Explore</Button>
-        <Button type={SECONDARY}>Explore</Button><br></br>
-
-        <ProductsList>
-          <ProductCard name={'Amazon Echo Dot'} rating={4} numberOfRaters={140} subText={'Sub line text'} bestSelling={true} imageUrl={'https://images-na.ssl-images-amazon.com/images/I/910HREIJBdL._SL1500_.jpg'}/>
-        </ProductsList>
-
+        <section>
+          <Products />
+        </section>
+        <br />
       </div>
     </Container>
   </div>
