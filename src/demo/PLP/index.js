46ac1b368b9997b785fa74fbc828032ca22205a3
Voleti, Shashikanth (US - Hyderabad) <shvoleti@deloitte.com>
2019-02-26 21:09:33 +0530

    pagination change

--- src/demo/PLP/index.js
+++ src/demo/PLP/index.js
@@ -11,19 +11,70 @@ export default class PLPComponent extends Component{
     constructor(){
         super();
         this.state={
-            productList:[]
+            productList:[],
+            displayList:[]
+        }
+        this.p1=1;
+        this.p2=2;
+        this.p3=3;
+        this.pageLength=12;
+        this.minPrev=0;
+        this.maxNext=0;
+
+        this.updatesOnPaginationNumber=(pageNo)=>{
+            let tempList=[];
+            const pageStart = this.pageLength*(pageNo-1),
+                  pageEnd   = pageStart+this.pageLength;
+
+            tempList = this.state.productList.slice(pageStart,pageEnd);
+
+            this.setState(
+                {
+                    displayList:tempList   
+                }
+            )
+        }
+
+        this.updatesPagniation=(paginationMovement)=>{
+            if(paginationMovement.toLowerCase()==='next' && this.p3<=this.maxNext){
+                this.p1+=1;
+                this.p2+=1;
+                this.p3+=1;
+
+                this.updatesOnPaginationNumber(this.p1);
+
+                return
+               
+            }
+
+            if(paginationMovement.toLowerCase()==='prev' && this.p1>=this.minPrev){
+                this.p1-=1;
+                this.p2-=1;
+                this.p3-=1;
+
+                this.updatesOnPaginationNumber(this.p1);
+
+                return
+            }
+
+           
+
         }
     }
 
     componentDidMount(){
 
-        let plpObj = this;
         axios.get(endPointUrl.productsList)
             .then(response => {
                 console.log('response-->',response);
-                plpObj.setState({
+                this.setState({
                     productList: response.data
-                })
+                });
+                
+                this.minPrev = this.state.productList.length>12?1:0;
+                this.maxNext = Math.ceil(this.state.productList.length/this.pageLength);
+
+                this.updatesOnPaginationNumber(1);
             })
             .catch(
                 error => {
@@ -35,15 +86,27 @@ export default class PLPComponent extends Component{
 
     render(){
         let productsList=[];
-        for(let i=0; i<this.state.productList.length;i++){
+        for(let i=0; i<this.state.displayList.length;i++){
             productsList.push(
-                <ProductCardComponent product={this.state.productList[i]} key={i}/>)
+                <ProductCardComponent product={this.state.displayList[i]} key={i}/>)
         }
 
         return(
         <div>
             {productsList}
+
+
+              { (this.p1)&& 
+              <div>
+                    <span onClick={()=>{this.p1>1&&this.updatesPagniation('prev')}}>Prev</span>
+                    <span onClick={()=>{this.updatesOnPaginationNumber(this.p1)}}>{this.p1}</span>
+                    {(this.p2<=this.maxNext)&&<span onClick={()=>{this.updatesOnPaginationNumber(this.p2)}}>{this.p2}</span>}
+                    {(this.p3<=this.maxNext)&&<span onClick={()=>{this.updatesOnPaginationNumber(this.p3)}}>{this.p3}</span>}
+                    <span onClick={()=>{this.p3<this.maxNext&&this.updatesPagniation('next')}}>next</span>
+                </div>
+            }
             </div>
+           
             )
     }
 }
