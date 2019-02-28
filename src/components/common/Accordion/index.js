de6968d0406dcfcc974d05c79d0f6214b5ed034d
Kaligatla, Praveen (US - Hyderabad) <pkaligatla@deloitte.com>
2019-02-28 12:51:42 +0530

    save

--- src/components/common/Accordion/index.js
+++ src/components/common/Accordion/index.js
@@ -5,17 +5,34 @@ import { UncontrolledCollapse } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 class Accordion extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            activeId: {
+                "electronics": false,
+                "television": false
+            }
+        }
+        this.setActiveElement = this.setActiveElement.bind(this);
+    }
+    setActiveElement(id){
+        const activeId = this.state.activeId;
+        activeId[id] = !activeId[id];
+        this.setState({
+            activeId
+        });
+    }
     render() {
         const { } = this.props;
         return (
             <section className="accordion">
                 <ul className="list-unstyled accordion-list">
                     <li>
-                        <Link to="/" id="electronics">Electronics</Link>
+                        <Link to="/" id="electronics" className={this.state.activeId['electronics']? "active" : ""} onClick={() => this.setActiveElement('electronics')}>Electronics</Link>
                         <ul className="list-unstyled child">
                             <li>
                                 <UncontrolledCollapse toggler="#electronics">
-                                    <Link to="/" id="television">Telivision</Link>
+                                    <Link to="/" id="television" className={this.state.activeId['television']? "active" : ""} onClick={() => this.setActiveElement('television')}>Telivision</Link>
                                     <ul className="list-unstyled child">
                                         <li>
                                             <UncontrolledCollapse toggler="#television">
